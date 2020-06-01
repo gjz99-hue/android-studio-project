3d76c58c20e116c95822b310af12c9b9c8ef4154
gjz99-hue <1677782551@qq.com>
2020-06-01 19:33:02 +0800

    项目1

---
+++ project1/app/src/main/java/com/example/mywechat/weixinFragment.java
@@ -0,0 +1,31 @@
+package com.example.mywechat;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class weixinFragment extends Fragment {
+
+
+    public weixinFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab01, container, false);
+    }
+
+}
